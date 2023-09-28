c7ec9ac47938d6793a4ab220e81274e8a8acec14
rmackzsp <70037805+rmackzsp@users.noreply.github.com>
2023-09-28 12:08:14 -0700

    Add files via upload

---
+++ Day 06 Letter.md
@@ -0,0 +1,3 @@
+Day 06 Letter
+
+So I wrote the letter how it said to write it in the book, but I have having a problem with it.  It didn’t turn out like It was supposed too.  And on top of that, I can’t seem to figure out how to make it run.  Looks like a I have a lot more to figure out on this one. 
---
+++ myletter.py
@@ -0,0 +1,33 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> spaces = ' ' * 25
+>>> print ('{} 12 Butts Wynd')
+{} 12 Butts Wynd
+>>> print ('{} Twinklebotttom Health')
+{} Twinklebotttom Health
+>>> print ('{} West Snoring')
+{} West Snoring
+>>> print ()
+
+>>> print ()
+
+>>> print ('Dear Sir')
+Dear Sir
+>>> print ()
+
+>>> print ('I wish to report that tile are missing from the')
+I wish to report that tile are missing from the
+>>> print('outside toilet roof.')
+outside toilet roof.
+>>> print('I think it was bad wind the other night that blew them away.')
+I think it was bad wind the other night that blew them away.
+>>> print('outside toilet roof.')
+outside toilet roof.
+>>> print ('I think it was bad wind the other night that blew them away.')
+I think it was bad wind the other night that blew them away.
+>>> print()
+
+>>> print('Regards')
+Regards
+>>> print('Malcom Dithering')
+Malcom Dithering
